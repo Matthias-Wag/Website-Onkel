0ce001793b74614c6698a5db2ddf5580e3c3ecbe
turowolff <107475409+turowolff@users.noreply.github.com>
2025-04-21 14:14:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburgerMenu = document.querySelector(".hamburger-menu");
     const nav = document.querySelector("header nav");
+    const navLinks = document.querySelectorAll("header nav ul li a");
 
+    // Toggle Navigation beim Klicken auf das Hamburger-Menü
     hamburgerMenu.addEventListener("click", () => {
         nav.classList.toggle("active");
+    });
+
+    // Navigation schließen, wenn ein Link angeklickt wird
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
     });
 });
